3b70528e341a8c144aa9a221b5dce156b2097270
AbdElrhman <62687698+master-pentesting@users.noreply.github.com>
2021-02-09 14:28:33 +0200

    update for the scan tool

--- base/bin/scan.py
+++ base/bin/scan.py
@@ -1,30 +1,37 @@
-from N4Tools.Design import Text,Square
+from N4Tools.Design import Text,Square,ThreadAnimation,Animation,AnimationTools
 import requests as req
 import socket,os,time,sys
-from threading import Thread
-import signal
-class Main:
+from threading import Thread as u
+A = Animation()
+class MA:
+	def CustomAnimation(min=0,max=5639,**kwargs):
+                yield A.Prograsse(min=min,max=max,prograsse=['│','\033[1;36m█','\033[1;34m▒','│'],text='Scanning',**kwargs)[0]+f'\033[1;37m({round(min*100/max,1)}/100.0) '
+
+class Main(MA):
+	ips=[]
 	def __init__(self):
 		try:
 			self.Sq = Square()
 			self.Sq.color = '[$LCYAN]'
 			self.T = Text()
-			self.eip = req.get('https://api.ipify.org').text
-			self.work = True
+			eip = req.get('https://api.ipify.org').text
+			self.ips.append(eip+'+eip')
 			s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 			s.connect(("8.8.8.8", 80))
-			self.iip = s.getsockname()[0]
-			s.close()
+			iip = s.getsockname()[0]
+			self.ips.append(iip+'+iip')
 			self.open_ports1 = []
 			self.open_ports1.sort()
 			self.open_ports2 = []
 			self.open_ports2.sort()
-			self.ports=set(list(range(1,6535)) + [8080,7547,6666,8888,7777,5555])
+			self.ports=set(list(range(1,6535)) + [8080,7547,6666,8888,7777])
 			self.mw=os.get_terminal_size().columns
 		except socket.gaierror:
 			exit()
 		except socket.error:
 			print('\033[1;31m[-] Check your internet connection..!\033[0m')
+			exit()
+		except KeyboardInterrupt:
 			exit()
 		b='''
 	   _____		   _--_
@@ -35,30 +42,17 @@ class Main:
 			           |""|
                         '''
 		print('\033[0;32m',b,'\033[0m')
-	def scan(self,ip,a):
-		myl=self.open_ports1 if a=='eip' else self.open_ports2
-		for port in self.ports:
-			s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.settimeout(0.1)
-			if s.connect_ex((ip,port))==0:
-				myl.append(port)
-		self.work = False
+
+
 	def serv(self,p):
 		try:
 			x=socket.getservbyport(p)
 		except socket.error:
 			x='Unknown'
 		return x
-	def bscan(self):
-		ban1,ban2 = ["•","••","•••","••••"],["/","-","\\","|"]
-		while self.work:
-			for x in range(4):
-				if not self.work:
-					break
-				sys.stdout.write("\033[1;37m\r[\033[1;31m%s\033[1;37m]\033[1;32m Scaning\033[1;33m %s "%(ban2[x%4],ban1[x%4]))
-				time.sleep(0.1)
-				sys.stdout.flush()
-	def display(self,i,a):
+
+	def display(self,i):
+		i,a=i.split('+')
 		myl=self.open_ports1 if a=='eip' else self.open_ports2
 		fu = '''
     port    Service    Status
@@ -74,22 +68,32 @@ class Main:
 		for x in output.split('\n'):
 			print("\t"+x)
 
-	def run(self):
-		p1=Thread(target=self.scan,args=(self.eip,'eip'))
+	@ThreadAnimation(Animation=MA.CustomAnimation)
+	def scan(Thread,self):
+		p=0
+
+		for ip in self.ips:
+			i,a=ip.split('+')
+			myl=self.open_ports1 if a=='eip' else self.open_ports2
+			for port in self.ports:
+				s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.settimeout(0.1)
+				if s.connect_ex((i,port))==0:
+					myl.append(port)
+				Thread.set_kwargs(min=p+1, max=6539*2)
+				p+=1
+		Thread.kill()
+
+	def d(self):
+		for ip in self.ips:
+			self.display(ip)
+
+	def runs(self):
+		p1=u(target=self.scan,args=())
 		p1.start()
 		p1.join()
-		p2=Thread(target=self.scan,args=(self.iip,'iip'))
-		p2.start()
-		p2.join()
-		s1=Thread(target=self.display,args=(self.eip,'eip'))
-		s2=Thread(target=self.display,args=(self.iip,'iip'))
-		s1.start();s1.join()
-		s2.start();s2.join()
-	def main(self):
-		zm2=Thread(target=self.run,args=())
-		zm2.start()
-		zm1=Thread(target=self.bscan,args=())
-		zm1.start()
+		s1=u(target=self.d,args=())
+		s1.start()
+
 if __name__ == '__main__':
-	myrun = Main()
-	myrun.main()
+	Main().runs()
