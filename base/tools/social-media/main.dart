2e238245d49a5ed61bb9c726568b25b81772c128
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2021-02-15 22:14:30 +0100

    update user-info and rename it to social-media

---
+++ base/tools/social-media/main.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:sprintf/sprintf.dart' as sprintf;
+import 'package:http/http.dart' as http;
+
+class SocialMedia{
+  var userName;
+
+  SocialMedia(userName) {
+    this.userName = userName;
+  }
+
+  void startLoop(){
+    new File('data.json').readAsString().then((String contents) {
+      var jsonData = json.decode(contents);
+      jsonData.forEach((hostName,Info){
+        var url = sprintf.sprintf(Info["url"],[this.userName]);
+        this.getRequest(url).then((value) {
+          if (value) {
+            print("[\x1B[32m+\x1B[0m] \x1B[32m$hostName: \x1B[0m$url");
+          }
+        }).catchError((e){
+        });
+      });
+    });
+  }
+
+  Future<bool> getRequest(url) async {
+    var req = await http.get(url);
+    if (req.statusCode == 200) {
+      return true;
+    }
+    return false;
+  }
+}
+
+String input(text){
+  stdout.write(text);
+  return stdin.readLineSync();
+}
+
+void main() {
+  var username = input('\x1B[33mUsername\x1B[32m~\x1B[31m/\x1B[0m\$ ');
+  SocialMedia Obj = new SocialMedia(username);
+  Obj.startLoop();
+}
