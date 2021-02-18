02df0ccf049632d818661aa417b16a640a416149
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2021-02-18 20:22:02 +0100

    update server

--- base/HackerModeServer/app.py
+++ base/HackerModeServer/app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template
+from rich.traceback import pretty
+pretty.install()
 
 working_dir = os.path.abspath(os.path.dirname(__file__))
 template_dir = os.path.join(working_dir, 'templates')
@@ -9,18 +11,24 @@ app = Flask(
     template_folder=template_dir,
     static_folder=static_dir
 )
-
+# https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxCEvFAEe3ziBvRL7ID-IijbniFupiKJS_nA&usqp=CAU
 @app.route('/' , methods=['POST','GET'])
-def index():
+def index_page():
     return render_template('index.html')
 
-@app.route('/signin' , methods=['POST','GET'])
-def signup():
-    return render_template('index.html')
+@app.route('/login' , methods=['POST','GET'])
+def login_page():
+    return render_template('login.html')
 
 @app.route('/signup' , methods=['POST','GET'])
-def signin():
-    return render_template('index.html')
+def signup_page():
+    return render_template('signup.html')
+
+@app.route('/about' , methods=['POST','GET'])
+def about_page():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        debug=True,
+    )
