0782d577448090264eeb4f48a5cc95615f6b1ad2
MLT <60033959+Mahmoud7Osman@users.noreply.github.com>
2021-02-20 12:11:13 -0500

    Add files via upload

---
+++ base/bin/aioc.sh
@@ -0,0 +1,48 @@
+path=$(pwd)
+
+gcc $path/base/services/scr/or_send.c -o path/base/services/scr/or_send
+gcc $path/base/services/scr/or_server.c -o path/base/services/scr/or_server
+gcc $path/base/services/scr/remote.c -o path/base/services/scr/remote
+
+if [ "$1" == "remote" ];then
+   printf "Enter Server IP---: "
+   read IP
+   printf "Enter Server Port-: "
+   read PORT
+   printf "Service Started, "
+   (scr/remote $IP $PORT &)
+   printf "To Stop Kill the proccess with command 'kill' \n"
+elif [ "$1" == "redout" ];then
+   printf "Enter Program Name To Run--: "
+   read prog
+   printf "Enter Server (Receiver) IP-: "
+   read IP
+   printf "Enter Server Port----------: "
+   read PORT
+   printf "Program $prog Started, Output redirected to $IP\n"
+   scr/redout $prog $IP $PORT
+elif [ "$1" == "recout" ];then
+   printf "Your IP-----------------: "
+   read ip
+   printf "Your Port---------------: "
+   read port
+   printf "Filname To store output-: "
+   read file
+   printf "Service Started, ($ip:$port)\n\n"
+   scr/recout $ip $port $file
+elif [ "$1" == "remcon" ];then
+   printf "Enter Your IP------: "
+   read iP
+   printf "Enter Port---------: "
+   read Port
+   printf "Waiting for Remote HackerMode Service to Connect...\n"
+   nc -lt $iP $Port
+else
+   printf "|----------------------|\n"
+   printf "Usage: aioc <command>\n"
+   printf "       aioc remote\n"
+   printf "       aioc recout\n"
+   printf "       aioc redout\n"
+   printf "       aioc remcon\n"
+   printf "|----------------------|\n"
+fi
