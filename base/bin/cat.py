6c62643711aec60ecc2d9bb621eb2b3f3b54eaa5
G705 <78649813+G705-Ghilan@users.noreply.github.com>
2021-03-18 20:22:53 +0300

    reader file.*

---
+++ base/bin/cat.py
@@ -0,0 +1,3 @@
+from N4Tools.Design import ThreadAnimation                                                                            import os,sys
+reader     = lambda Thread,path : os.popen(f'pygmentize {path}').read()
+if len(out:=sys.argv)>1         : print (ThreadAnimation()(reader)(' '.join(out[1:])))
