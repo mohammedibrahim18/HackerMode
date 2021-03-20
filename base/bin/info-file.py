2a75ee2366d415dea2a05d854e7ce444ba2f2922
G705 <78649813+G705-Ghilan@users.noreply.github.com>
2021-03-20 14:15:44 +0300

    Update G705

--- base/bin/info-file.py
+++ base/bin/info-file.py
@@ -1,157 +1,230 @@
-from N4Tools.Design import Color,ThreadAnimation
-import os,json,sys
+import os,re,json
+from N4Tools.Design import ThreadAnimation
+from rich.table import Table
+from rich.live import Live
+from rich import box
+from time import sleep
+import sys, os
+
 sys.path.append(os.path.abspath(__file__).split('/bin')[0])
 from shell import BaseShell
-path=input(Color().reader('[$LYELLOW]Path[$GREEN]~[$LRED]/[$LWIHTE]$ [$WIHTE]'))
 
-if not os.path.isdir(path):print(f"[Errno 20] Not a directory: '{path}'");exit()
-TOOL_NAME = __file__.split('/')[-1].split('.')[0]
-def getsize(num):
-	G=num/1024
-	tmp="KB"
-	if G>1024:G,tmp=G/1024,'MB'
-	if G>1024:G,tmp=G/1024,'GB'
-	G=str(G).split('.')
-	return f"{G[0]}.{G[1][0:2]} {tmp}"
+class GetInfo:
+    def __init__(self,path):
+        self.path = path
+    def getsize(self,num):
+        G = num / 1024
+        tmp = "KB"
+        if G > 1024 :
+            G,tmp = G / 1024, 'MB'
+        if G > 1024 :
+            G,tmp = G / 1024, 'GB'
+        G = str(G).split('.')
+        return f"{G[0]}.{G[1][0:2]} {tmp}"
+    def dictinfo(self):
+        #Mode      -> { NameFile : [ repeat , sizeAllrepeat , { NameFile : SizeFile } ] }
+        #rest      -> [ SizeAll , { NameFile : SizeFile }]
+        #in_repeat -> { NumSize  : {Num:[ Paths] } } 
+        data={'Mode':{},
+              'rest':[0, {}],
+              'in_repeat': None}
+        in_repeat = {}
+        In_Repeat = {}
+        def addrest(n, k, v):
+            data['rest'][0] += n
+            data['rest'][1][k] = v
+
+        for p,d,f in os.walk(self.path):
+            for Mode in f:
+                path = os.path.join(p,Mode)
+                size = os.path.getsize(path)
+                get_size = self.getsize(size)
+                #a=Mode.rfind('.')
+                out = Mode[Mode.rfind('.'):][1:]
+                if out:
+                    if not (len([x for x in (re.findall('[\W]*',out))if x])>0) \
+                    and '_' not in out and not out.isnumeric():
+                        if out not in data['Mode']:
+                            data['Mode'][out] = [1, size, {path: get_size}]
+                        if out not in In_Repeat:
+                            In_Repeat[out] = {}
+                        if size not in in_repeat:
+                            in_repeat[size] = path
+                        else:
+                            if size in In_Repeat[out]:
+                                In_Repeat[out][size] += [in_repeat[size], path]
+                            else:
+                                In_Repeat[out][size] = [in_repeat[size], path]
+                            In_Repeat[out][size] = list(set(In_Repeat[out][size]))
+                        data['Mode'][out][0] += 1
+                        data['Mode'][out][1] += size
+                        data['Mode'][out][2][path] = get_size
+#                        else:data['Mode'][out]=[1,size,{path:get_size}]
+                    else:
+                        addrest(size, path, get_size)
+                else:
+                    addrest(size, path, get_size)
+        Mode={}
+        for x in sorted(data['Mode']):
+            info = data['Mode'][x]
+            Mode[x] =data['Mode'][x] # [info[0], self.getsize(info[1]) ,info[2]]
+        data['Mode'] = Mode
+        data['rest'][0] = self.getsize(data['rest'][0])
+        In_Repeat = {x: In_Repeat[x] for x in sorted(In_Repeat)}
+        data['in_repeat'] = In_Repeat
+        return data
 
 @ThreadAnimation()
-def getnumpers(Thread):
-	all={}
-	for d,i,r in os.walk(path):
-		for n in r:
-			a=str(n[n.rfind('.'):][1:])
-			p=(os.path.join(d,n))
-			s=os.path.getsize(p)
-			size=getsize(s)
-			if a in all:all[a].append((p,size,s))
-			else:all[a]=[(p,size,s)]
-	Thread.kill=True
-	return all
+def GETdata(Thread, path):
+    data = GetInfo(path).dictinfo()
+    Thread.kill = True
+    return data
+
+class Input(BaseShell): #input...
+      def __init__(self, P, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.P = P
+        self.prompt = P
+        self.value = '.'
+        self.cmdloop()
+      do_dir = lambda self, arg: 'pass'
+      completenames = lambda self, *args: ['dir ']
+      def postcmd(self, *args):
+          self.prompt = self.P
+          if os.path.isdir(self.value):
+              return True
+          else:
+              print('Not Dir...!')
+      def onecmd(self, line):
+          line = line[line.find(' '):].strip()
+          self.value = line
 
 
+Data = GETdata(Input('\033[1;33mPath\033[1;36m~\033[1;31m/\033[1;37m$\033[0m').value)
 
-class NumFile(BaseShell):
-	ToolName=TOOL_NAME
-	all=getnumpers()
-	DoneSearch={}
-	for x in all.keys():
-		if ' ' in x or '-' in x or '_' in x or x.isnumeric():pass
-		else:
-			exec(f'''\
-			\rdef do_{x}(self,arg):
-			@ThreadAnimation()
-			def th(Thread):
-				out=''
-				if not arg:out=''
-				elif arg=='num':
-					out=(Color().reader("[$LYELLOW]Numper:[$LGREEN] ")+str(len(self.all['{x}'])))
-				elif arg=='sizeall':out=Color().reader('[$LYELLOW]Sizeall:[$LGREEN] ')+getsize(sum([x[-1] for x in self.all['{x}']]))
-				
-				elif arg=='paths':
-					for x in self.all["{x}"]:
-						out+=Color().reader("[$LGREEN]"+x[0]+" [$LYELLOW]Size: [$LRED]"+x[1]+'\\n')
-				Thread.kill=True
-				return out[:-1] if out.endswith('\\n') else out
-			if arg=='repeat':self.lops('{x}')
-			elif arg=='delrepeat':self.lops('{x}',mode='rm')
-			elif arg=='delpaths':self.delpaths("{x}")
-			else:
-				G=(th())
-				if G:print(G)
-			\rdef complete_{x}(self,arg,*args):
-			a=['paths','repeat','sizeall','num','delrepeat','delpaths']
-			if not arg:return a
-			else:return [x for x in a if x.startswith(arg)]
-			''')
-	def Lexer(self,text):###
-		a=highlight(text,JsonLexer(), TerminalFormatter())
-		return('\r'+a)
-	def delpaths(self,arg):
-		point=lambda t:Color().reader(f'[$LYELLOW]Loading... [$LRED][ [$GREEN]{t}[$YELLOW]%[$GREEN]100 [$LRED]]')
-		rm=0
-		for x in self.all[arg]:
-			os.system(f'rm {x[0]}')
-			print(point(str(rm/len(self.all[arg])*100).split('.')[0]),end='\r')
-			rm+=1
-		print(point('100'),end='\r')
-		
-		self.all[arg]=[]
-		print(' '*len(Color().del_colors(point('100'))),end='\r')
-		print((Color().reader(f"[$LYELLOW]DelPaths:[$LGREEN] {rm}")))
-	def lops(self,arg,mode='show'):
-		point=lambda t:Color().reader(f'[$LYELLOW]Loading... [$LRED][ [$GREEN]{t}[$YELLOW]%[$GREEN]100 [$LRED]]')
-		
-		if arg in self.DoneSearch:
-			if mode=='show':(self.styledata(self.DoneSearch[arg][0],self.DoneSearch[arg][1]));return None
-			elif mode=='rm':
-				self.Remove(arg,self.DoneSearch[arg][-1]);return None
-				
+class Main(BaseShell):
+    ToolName = "Info-File"
+    remove = []
+    for x in Data['Mode'].keys():
+        exec(f'''
+def do_{x}(self, arg):
+    if arg == 'paths' :
+        self.paths("{x}")
+    elif arg == "Del_paths" :
+        self.paths("{x}", Del=True)
+    elif arg == "size_all" :
+        print ('\033[0;33mSize_All \033[1;31m: \033[0;32m',GetInfo(".").getsize(Data["Mode"]["{x}"][1]))
+    elif arg == 'repeat' :
+        self.repeat("{x}")
+    elif arg == 'Del_repeat' :
+        self.repeat("{x}", Del=True)
+def complete_{x}(self, arg, *args):
+    all=['repeat', 'size_all',
+         'Del_repeat','paths',
+         'Del_paths'
+        ]
+    return all if not arg else [x for x in all if x.startswith(arg)]''')
+    def paths(self, arg, Del=False):
+        table = Table(
+                    expand=True, box=box.SQUARE_DOUBLE_HEAD,
+                    title="Paths", title_style="none")
+        table.add_column('Path', style="green")
+        table.add_column('Size', style='blue')
+        if Del:
+            table.add_column('Del', style="red")
+        all = 0
+        with Live(table, refresh_per_second=1)as live:
+            for k,v in Data['Mode'][arg][2].items():
+                if k not in self.remove:
+                    if not Del:
+                        table.add_row(k, str(v))
+                    else:
+                        table.add_row(k, str(v), str(self.Remove(k)))
+                    all += 1
+        print (f'\033[0;33m{"Del" if Del else "Path"}_All\033[1;31m : \033[0;32m{all}\n\033[0;33mSize_All \033[1;31m: \033[0;32m{GetInfo(".").getsize(Data["Mode"][arg][1])}')
+        if Del:
+            Data['Mode'][arg][2] = {}
+            Data['Mode'][arg][1] = 0
+    def Remove(self, path):
+        try:
+            os.remove(path)
+            return True
+        except:
+            return False
+    def reader(self, path):
+        with open(path, 'rb')as f:
+            return f.read()
 
-		print(point('0'),end='\r')
-		tmp,done,all,Num,i,rm,info=[],[],self.all[arg],0,0,0,[]
-		All=list(self.all[arg])
-		Doneremove=[]
-		if mode=='rm':rem=list(self.all[arg])
-		for x in all:
-			try:
-				with open(x[0],'rb')as f:tmp1=f.read()
-				t=[]
-				if x[0]not in done:
-					for d in All:
-						if not d[0]==x[0]:
-							with open(d[0],'rb')as ff:tmp2=ff.read()
-							if tmp1==tmp2:
-								
-								
-								t.append(d[0]);i+=1 #tmp repeat
-								done.append(d[0]) #Done repeat
-								info.append(d)
-								#elif mode=='rm':
-								All.remove(d) #remove is done repeat
-								
-					if len(t+[x[0]])>1:tmp.append(t+[x[0]])
-					print(point(str(Num/len(self.all[arg])*100).split('.')[0]),end='\r')
-					Num+=1
-			except FileNotFoundError as e:print(e)
-		print(point('100'),end='\r')
-		print(' '*len(Color().del_colors(point('100'))),end='\r')
-		if mode=='show':
-			if tmp:
-				self.DoneSearch[arg]=[tmp,done,info]
-				self.styledata(tmp,done)
-			else:print((Color().reader("[$LYELLOW]Repeat:[$LGREEN] 0")))
-		elif mode=='rm':
-			self.Remove(arg,info)
-			
-	def styledata(self,tmp,done):
-		if done==True:print((Color().reader("[$LYELLOW]Repeat:[$LGREEN] 0")));return None
-		string=''
-		for x in tmp:
-			for p in range(len(x)):
-				if p==0:string+='[$LYELLOW]╭[$LRED][[$LGREEN]'+x[p]+'[$LRED]]\n'
-				elif p==len(x)-1:string+='[$LYELLOW]╰[$LRED][[$LGREEN]'+x[p]+'[$LRED]]\n'
-				else:string+='[$LYELLOW]┝[$LRED][[$LGREEN]'+x[p]+'[$LRED]]\n'
-		Repeat=((Color().reader("[$LYELLOW]Repeat:[$LGREEN] ")+str(len(done))))
-		out=Color().reader(string)+Repeat
-		print(out)
-	def Remove(self,arg,a):
-		point=lambda t:Color().reader(f'[$LYELLOW]Loading... [$LRED][ [$GREEN]{t}[$YELLOW]%[$GREEN]100 [$LRED]]')
-		print(point('0'),end='\r')
-		i=0
-		rem=list(self.all[arg])
-		for x in a:
-			os.remove(x[0])
-			rem.remove(x)
-			print(point(str(i/len(a)*100).split('.')[0]),end='\r')
-			i+=1
-		print(point('100'),end='\r')
-		self.all[arg]=rem
-		self.DoneSearch[arg]=[[],True,[]]
-		print(' '*len(Color().del_colors(point('100'))),end='\r')
-		print((Color().reader("[$LYELLOW]DelRepeat:[$LGREEN] ")+str(i)))
-		
-	def do_main(self,arg):
-		return True
-NumFile().cmdloop()
-	
-	
+    def repeat(self, arg, Del=False):
+        all = Data['in_repeat'][arg]
+#        if all == {} :
+#            print ('Not Repeat')
+#            return False
+        k = list(all.keys())
+        P = []
+        Done = []
+        table = Table(
+                    expand=True, box=box.SQUARE_DOUBLE_HEAD,
+                    title="Repeat", title_style='none')
+        table.add_column('Path', style='green')
+        table.add_column('Size', style='blue')
+        table.add_column("Mode", style="cyan")
+        if Del:
+            table.add_column('Del', style="red")
+        size_all = 0
+        Del_all = 0
+        size_all_repeat = 0
+        D = ''
+        with Live(table)as live:
+            a=[0]
+            repeat_all=0
+            for k,v in all.items():
+                a[0]+=k
+                a.append([k,v])#a.append([GetInfo('.').getsize(k),v])
+            #print (json.dumps(a,indent=2))
+            for x in a[1:]:
+                S=GetInfo('.').getsize(x[0])
+                if len(x[1]) == 2:
+                    if self.reader(x[1][0]) == self.reader(x[1][1]):
+                        size_all_repeat += x[0]
+                        repeat_all += 1
+                        if Del:
+                           table.add_row(x[1][0] , S, "orignal","False")
+                           table.add_row(x[1][1] , S, "plus",str(self.Remove(x[1][0]))) 
+                           Del_all += 1
+                           Data["Mode"][arg][1] -= x[0]
+                           try:
+                               self.remove.append(x[1][1])
+                               x[1].remove(x[1][1])
+                           except:pass
+                           repeat_all += 1
+                        else:
+                            table.add_row(x[1][0] , S, "orignal")
+                            table.add_row(x[1][1] , S, "plus")
+                elif len(x[1]) > 2:
+                    original=x[1][0]
+                    r=self.reader(original)
+                    N = 0
+                    for p in x[1][1:]:
+                        if r == self.reader(p):
+                            if N==0:
+                                if Del:
+                                    table.add_row(original, S, "original", "False")
+                                    #x[1].remove(original)
+                                else:
+                                    table.add_row(original, S, "original")
+                            if Del:
+                                table.add_row(p,S,"plus",str(self.Remove(p)))
+                                Del_all += 1
+                                Data["Mode"][arg][1] -= x[0]
+                                x[1].remove(p)
+                                self.remove.append(p)
+                            else:
+                                table.add_row(p,S,"plus")
+                            size_all_repeat += x[0]
+                            N+=1
+                            repeat_all+=1
+                    live.update(table)
+        print (f'\033[0;33m{"Del" if Del else "Repeat"}_All\033[1;31m :\033[0;32m {Del_all if Del else repeat_all}\n\033[0;33mSize_All_{"Del"if Del else "Repeat"} \033[1;31m:\033[0;32m {GetInfo(".").getsize(size_all_repeat)}')
+Main().cmdloop()
+
