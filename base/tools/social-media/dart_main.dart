eb1bad3a81bd7cc805fc3fa1a3cb467c091bdedd
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2021-04-23 02:42:51 +0200

    speed up++

--- base/tools/social-media/dart_main.dart
+++ base/tools/social-media/dart_main.dart
@@ -3,37 +3,52 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class SocialMedia{
-  var userName;
+  String userName;
+  List urls200 = [];
+  int _counter = 0;
+  Map<String,dynamic> contentsAsMap = {};
 
-  SocialMedia(userName) {
-    this.userName = userName;
-  }
+  SocialMedia(this.userName);
 
-  void startLoop(){
-    // to start search
-    new File('data.json').readAsString().then((String contents) {
-      var jsonData = json.decode(contents);
-      jsonData.forEach((hostName,Info){
-        var url = Info["url"].replaceAll("%s",this.userName);
-        url = Uri.parse(url);
-        this.getRequest(url).then((value) {
-          if (value) {
-            print("[\x1B[32m+\x1B[0m] \x1B[32m$hostName: \x1B[0m$url");
-          }
-        }).catchError((e){
-        });
+  void startLoop() async {
+    String contents = await File('data.json').readAsString();
+    this.contentsAsMap = json.decode(contents);
+    contentsAsMap.forEach((hostName,info){
+      String url = info["url"].replaceAll("%s",this.userName);
+
+      this.getRequest(Uri.parse(url)).then((value) {
+        _counter++;
+        stdout.write("\r# Loading [$_counter/${contentsAsMap.keys.length}]");
+        if (value) {
+          this.urls200.add(hostName);
+        }
+        if (_counter == contentsAsMap.keys.length){
+          this.showData();
+        }
+
       });
     });
   }
 
   Future<bool> getRequest(url) async {
-    // To make request
-    var req = await http.get(url).timeout(
-      Duration(seconds: 3),);
-    if (req.statusCode == 200) {
-      return true;
+    try{
+      var req = await http.get(url).timeout(Duration(seconds: 3));
+      if (req.statusCode == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
     }
-    return false;
+  }
+
+  void showData(){
+    //print("[\x1B[32m+\x1B[0m] \x1B[32m$hostName: \x1B[0m$url");
+    this.urls200.sort();
+    this.urls200.forEach((hostName){
+    print("[\x1B[32m+\x1B[0m] \x1B[32m$hostName: \x1B[0m${contentsAsMap[hostName]["url"].replaceAll('%s',this.userName)}");
+    });
+    exit(-1);
   }
 }
 
@@ -46,5 +61,5 @@ String input(text){
 void main() async {
   var username = input('\x1B[33mUsername\x1B[32m~\x1B[31m/\x1B[0m\$ ');
   SocialMedia Obj = new SocialMedia(username);
-  await Obj.startLoop();
+  Obj.startLoop();
 }
