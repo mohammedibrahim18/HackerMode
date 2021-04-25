304d4adf42a20a6d922c2cbc617f5fbd16fa385a
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2021-04-25 15:46:36 +0200

    add delete

--- __main__.py
+++ __main__.py
@@ -42,8 +42,24 @@ class HackerMode:
 
     def check(self):
         Installer.check()
-
-
+    
+    def delete(self):
+        root_path = os.path.join(os.environ["_"].split("/bin/")[0]+"/bin/","sudo")
+        bin_path = os.path.join(os.environ["_"].split("/bin/")[0]+"/bin/","HackerMode")
+        tool_path = os.path.join(os.environ["HOME"],".HackerMode")
+        status = input("# Do you really want to delete the tool?\n [n/y]: ").lower()
+        if status in ("y","yes","ok","yep"):
+            root = ""
+            if os.path.exists(root_path):
+                root = "sudo"
+            if os.path.exists(bin_path).
+                os.system(f"{root} rm {bin_path}"
+            if os.path.exists(tool_path).
+                os.system(f"{root} rm -rif {tool_path}"
+            if not os.path.exists(tool_path) and not os.path.exists(bin_path):
+                print("# The deletion was successful...")
+            else:
+                print("# Error: could not delete the tool!")
 if __name__ == '__main__':
     HackerMode = HackerMode()
     HackerMode.start(sys.argv)
